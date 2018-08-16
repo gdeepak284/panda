b3da0f8b6da67745e968aa4dfc03e804429f1f54
gdeepak <gdeepak284@gmail.com>
2018-08-16 18:10:42 +0530

    removed encode

--- panda.py
+++ panda.py
@@ -1,4 +1,5 @@
 import xlrd
+import json
 
 
 def read_the_file(path='/Users/user/Downloads/input.csv'):
@@ -12,23 +13,23 @@ def read_the_file(path='/Users/user/Downloads/input.csv'):
 if __name__ == "__main__":
     book = xlrd.open_workbook('/Users/user/Downloads/ISO10383_MIC.xls')
     sheet = book.sheet_by_name('MICs List by CC')
-
-    # read header values into the list
-    keys = [str(sheet.cell(0, col_index).value) for col_index in xrange(sheet.ncols)]
-    print(keys)
+    keys = [str(sheet.cell(0, col_index).value) for col_index in range(sheet.ncols)]
 
     dict_list = []
-    for row_index in xrange(1, sheet.nrows):
-        d = {keys[col_index]: sheet.cell(row_index, col_index).value.encode("utf-8")
-             for col_index in xrange(sheet.ncols)}
+    for row_index in range(1, sheet.nrows):
+        d = {keys[col_index]: sheet.cell(row_index, col_index).value
+             for col_index in range(sheet.ncols)}
         dict_list.append(d)
 
-    # print dict_list
-    for i in range(len(dict_list) - 1665):
+    for i in range(4):
         print(dict_list[i])
-        print "\n"
+        print("\n")
 
-
-
-#     /Users/user/Downloads/input.csv
-#
+    # with open('/Users/user/PycharmProjects/Assignment/employees.json', 'r') as data_file:
+    #     employees = json.load(data_file)
+    #     s = json.dump(employees, dict_list)
+    #     print(s)
+    # # print dict_list
+    # # for i in range(4):
+    #     # print(dict_list[i])
+    #     # print "\n"
