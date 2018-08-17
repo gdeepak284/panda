e991573eaefdc08b26c9ed05cf60b34d1067ad28
gdeepak <gdeepak284@gmail.com>
2018-08-17 18:38:23 +0530

    Able to dowload xls file

--- panda.py
+++ panda.py
@@ -1,28 +1,30 @@
 import xlrd
-import json
-import boto3
+import json, requests
+import boto3, botocore
 from boto3.s3.transfer import S3Transfer
 
-
-aws_access_key_id= 'AKIAJLQJBMRC7YE6OWOQ'
-aws_secret_access_key= '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'
-bucket_name ='pandadeepak'
-
-
-def upload_url_to_s3():
-    client = boto3.client('s3',
-                          aws_access_key_id=aws_access_key_id,
-                          aws_secret_access_key=aws_secret_access_key)
-    transfer = S3Transfer(client)
-    transfer.upload_file('/Users/user/Desktop/Assignment',
-                         bucket_name,
-                         "employees.json")
+aws_access_key_id = 'AKIAJLQJBMRC7YE6OWOQ'
+aws_secret_access_key = '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'
+bucket_name = 'pandadeepak'
+xls_filepath = 'https://www.iso20022.org/sites/default/files/ISO10383_MIC/ISO10383_MIC.xls'
 
 
 if __name__ == "__main__":
-    book = xlrd.open_workbook('/Users/user/Downloads/ISO10383_MIC.xls')
+    s3 = boto3.client('s3',
+                      aws_access_key_id=aws_access_key_id,
+                      aws_secret_access_key=aws_secret_access_key)
+    # with open(xls_filepath, "wb") as f:
+    #     s3.download_fileobj(bucket_name, aws_access_key_id, f)
+    resp = requests.get(xls_filepath)
+    output = open('test.xls', 'wb')
+    output.write(resp.content)
+    output.close()
+
+    book = xlrd.open_workbook(
+        'https://s3.us-east-2.amazonaws.com/pandadeepak/ISO10383_MIC.xls')
     sheet = book.sheet_by_name('MICs List by CC')
-    keys = [str(sheet.cell(0, col_index).value) for col_index in range(sheet.ncols)]
+    keys = [str(sheet.cell(0, col_index).value) for col_index in
+            range(sheet.ncols)]
 
     dict_list = []
     for row_index in range(1, sheet.nrows):
@@ -33,10 +35,10 @@ if __name__ == "__main__":
     with open('employees.json', 'w') as fout:
         json.dump(dict_list, fout, indent=4)
 
-    client = boto3.client('s3',
-                          aws_access_key_id=aws_access_key_id,
-                          aws_secret_access_key=aws_secret_access_key)
-    transfer = S3Transfer(client)
+    s3 = boto3.s3('s3',
+                  aws_access_key_id=aws_access_key_id,
+                  aws_secret_access_key=aws_secret_access_key)
+    transfer = S3Transfer(s3)
     transfer.upload_file('/Users/user/Desktop/Assignment/employees.json',
                          bucket_name,
                          "employees.json")
